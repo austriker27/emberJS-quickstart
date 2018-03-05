778753ba8d97c9b55cd77b11b934021d460fa1c0
David Lindahl <davidalindahl@gmail.com>
2018-03-05 15:09:09 -0800

    more bears

---
+++ app/components/bear-list.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  bears: [],
+  actions: {
+    addBear: function() {
+      let bearName = this.get('bearName');
+      this.get('bears').pushObject(bearName);
+    }
+  }
+});
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,1 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  bears: ['grizzly', 'polar', 'brown'],
-  actions: {
-    addBear: function() {
-      let bearName = this.get('bearName');
-      this.get('bears').pushObject(bearName);
-    }
-  }
-});
+{{bear-list}}
